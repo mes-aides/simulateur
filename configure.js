3b24b04caffed1428635452c1ca58607135d18b7
Alexandre Rousseau <contact@rousseau-alexandre.fr>
2020-08-27 20:42:43 +0200

    remove dependencies & move to node 10

--- configure.js
+++ configure.js
@@ -16,46 +16,13 @@ module.exports = function(app) {
 
   app.use(bodyParser.urlencoded({ limit: '1024kb' }));
 
-  // Route to download a PDF
-  let puppeteerArgs = {};
-  if (process.env.PUPPETEER_ARGS) {
-      try {
-          puppeteerArgs = JSON.parse(process.env.PUPPETEER_ARGS);
-      } catch(e) {
-          // Do nothing
-      }
-  }
 
   app.set('trust proxy', true)
 
-  app.route('/foyer/resultat').post(function(req, res) {
-      var html = Buffer.from(req.body.base64, 'base64').toString('utf-8');
-
-      var pdfOptions = {
-          format: 'A4',
-          margin: {
-              top: '0.5cm',
-              right: '2cm',
-              bottom: '0.5cm',
-              left: '2cm'
-          }
-      };
-
-      var callback = function (pdf) {
-          res.writeHead(200, {
-              'Content-Type': 'application/pdf',
-              'Content-Disposition': 'attachment; filename=MesAides_simulation_' + req.body.basename + '.pdf',
-          });
-          res.end(pdf, 'binary');
-      };
-
-      utils.convertHTMLToPDF(html, callback, pdfOptions, puppeteerArgs, false);
-  });
 
 
   if (app.get('env') == 'development') {
       app.use(morgan('dev'));
-      app.use(require('errorhandler')());
   } else {
       app.use(morgan('combined'));
   }
