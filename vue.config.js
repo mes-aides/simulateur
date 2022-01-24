83e457576f5b2bd87a412b6bb09d17188fff495c
Thomas Guillet <thomas@palya.fr>
2022-01-24 20:15:50 +0100

    Adapte à mes-aides.org

--- vue.config.js
+++ vue.config.js
@@ -20,8 +20,8 @@ process.env.VUE_APP_BENEFIT_COUNT = benefits.all.filter(
 ).length
 process.env.VUE_APP_MATOMO_ID = matomo.id
 process.env.VUE_APP_VALIDATION_DELAY = animation?.delay || 0
-process.env.VUE_APP_CONTACT_EMAIL = "aides-jeunes@beta.gouv.fr"
-process.env.VUE_APP_CONTEXT_NAME = "1jeune1solution"
+process.env.VUE_APP_CONTACT_EMAIL = "equipe@mes-aides.org"
+process.env.VUE_APP_CONTEXT_NAME = "mes-aides.org"
 process.env.VUE_APP_BASE_URL = baseURL
 process.env.VUE_APP_CONTEXT = process.env.CONTEXT
 process.env.VUE_APP_PR_URL = `${process.env.REPOSITORY_URL}/pull/${process.env.REVIEW_ID}`
